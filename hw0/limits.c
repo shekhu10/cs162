3ddeba92afe2eafa7a138730b90d14079067ca5d
Shekhar <bhardwajshekhar10@gmail.com>
2020-05-08 10:46:15 +0000

    completed 3.2

--- hw0/limits.c
+++ hw0/limits.c
@@ -3,8 +3,18 @@
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    long int max_stack_size, max_no_of_process, max_file_descriptors; 
+    if (getrlimit(RLIMIT_STACK, &lim) == 0){
+        max_stack_size = lim.rlim_cur;
+    }
+    if(getrlimit(RLIMIT_NPROC, &lim) == 0){
+        max_no_of_process = lim.rlim_cur;
+    }
+    if (getrlimit(RLIMIT_NOFILE, &lim) == 0){
+        max_file_descriptors = lim.rlim_cur;
+    }
+    printf("stack size: %ld\n", max_stack_size);
+    printf("process limit: %ld\n", max_no_of_process);
+    printf("max file descriptors: %ld\n", max_file_descriptors);
     return 0;
 }
